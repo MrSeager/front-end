b13a39cc8b29de350dbdacc64cb20bd7aeee622e
MrSeager <seager1993@gmail.com>
2024-01-24 11:24:32 +0200

    1/24/2024

---
+++ Completed/loopstudios-landing-page-main/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.getElementById("navContainer").style.display = "flex";
+}
+
+function closeNav() {
+  document.getElementById("navContainer").style.display = "none";
+}
